64aeef2f1135920b20afb8246f2e14158da608d9
BeatrizPortella <84148961+BeatrizPortella@users.noreply.github.com>
2026-01-12 13:53:53 -0300

    Forçando IPv4 no banco de dados

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,32 +2,18 @@ module.exports = {
     apps: [
         {
             name: 'whatsapp-bot',
-            script: './src/bot.js',
-            instances: 1,
-            autorestart: true,
-            watch: false,
-            max_memory_restart: '500M',
-            env: {
-                NODE_ENV: 'production'
-            },
-            error_file: './logs/bot-error.log',
-            out_file: './logs/bot-out.log',
-            log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-            merge_logs: true
-        },
-        {
-            name: 'whatsapp-server',
             script: './src/server.js',
             instances: 1,
             autorestart: true,
             watch: false,
-            max_memory_restart: '500M',
+            max_memory_restart: '800M',
+            node_args: "--dns-result-order=ipv4first",
             env: {
                 NODE_ENV: 'production',
                 PORT: 3000
             },
-            error_file: './logs/server-error.log',
-            out_file: './logs/server-out.log',
+            error_file: './logs/error.log',
+            out_file: './logs/out.log',
             log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
             merge_logs: true
         }
